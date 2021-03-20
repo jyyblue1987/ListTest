1b5ec711df64bcb37bf2879fdcac6f21746e2b55
jyyblue <jyyblue1987@outlook.com>
2021-03-20 10:32:23 +0800

    [create] create project.

---
+++ src/main/java/lists/ArrayList.java
@@ -0,0 +1,172 @@
+package lists;
+
+import java.util.*;
+
+public class ArrayList<E> implements Iterable<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int size;
+    private E[] items;
+
+    public ArrayList() {
+        clear();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(Object o)
+    {
+        return (indexOf(o) != -1);
+    }
+
+    
+    public Iterator<E> iterator() {
+        return new ArrayListIterator();
+    }
+
+    public boolean add(E e) {
+        return add(size, e);        
+    }
+
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if( index < 0 )
+            return false;
+        
+        for (int i = index; i < size - 1; i++) {
+            items[i] = items[i + 1];
+        }
+        size--;
+        return true;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c){
+            if( add(e) == false )
+                return false;
+        }
+
+        return true;
+    }
+
+    public boolean addAll(int index, Collection<? extends E> c) {
+        int i = 0;
+        for (E e : c){
+            if( add(i, e) == false )
+                return false;
+            i++;
+        }
+
+        return true;
+    }
+
+    public void clear() {
+        size = 0;
+        ensureCapacity(DEFAULT_CAPACITY);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) throw new ArrayIndexOutOfBoundsException();
+
+        return items[index];
+    }
+
+    public E set(int index, E newVal) {
+        if (index < 0 || index >= size) throw new ArrayIndexOutOfBoundsException();
+        E old = items[index];
+        items[index] = newVal;
+        return old;
+    }
+
+    public boolean add(int index, E element) {
+        if( index > size )
+            return false;
+
+        if (items.length == size) ensureCapacity(size + 20);
+
+        for (int i = size; i > index; i--) {
+            items[i] = items[i - 1];
+        }
+        items[index] = element;
+        size++;
+
+        return true;
+    }
+
+
+    public int indexOf(Object o)
+    {
+        int result = -1;
+
+        for(int i = 0; i < size; i++)
+        {
+            if (o.equals(items[i]))
+            {
+                result = i;
+                break;
+            }         
+        }
+
+        return result;
+    }
+
+    public int lastIndexOf(Object o)
+    {
+        int result = -1;
+
+        for(int i = size - 1; i >= 0; i--)
+        {
+            if (o.equals(items[i]))
+            {
+                result = i;
+                break;
+            }         
+        }
+
+        return result;
+    }
+
+    public List<E> subList(int fromIndex, int toIndex)
+    {
+        List<E> sublist = new java.util.ArrayList<E>();
+        for(int i = fromIndex; i < toIndex; i++)
+        {
+            sublist.add(items[i]);
+        }
+
+        return sublist;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    private class ArrayListIterator implements java.util.Iterator<E> {
+
+        private int current = 0;
+
+        public boolean hasNext() {
+            return current < size;
+        }
+
+        public E next() {
+            if (!hasNext()) throw new java.util.NoSuchElementException();
+            return items[current++];
+        }
+    }
+
+    public void ensureCapacity(int newCapacity) {
+        if (newCapacity < size) 
+            return;
+
+        E[] old = items;
+        items = (E[]) new Object[newCapacity];
+
+        for (int i = 0; i < size; i++) {
+            items[i] = old[i];
+        }
+    }
+}
---
+++ src/test/java/test/ArrayListTest.java
@@ -0,0 +1,288 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import lists.ArrayList;
+import org.junit.Test;
+
+class ArrayListTest {
+
+	@Test
+	void add_ExpectListSizeToIncreaseBy1() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedSize = 1;
+
+		list.add(5);
+
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	void add_addTwo_ExpectListSizeToIncreaseBy2() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedSize = 2;
+
+		list.add(5);
+		list.add(10);
+
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	void add_addThree_ExpectThemAddedToEnd() {
+		ArrayList list = new ArrayList<Integer>();
+		List expectedOrder = Arrays.asList(5, 10, 15);
+
+		list.add(5);
+		list.add(10);
+		list.add(15);
+
+		assertEquals(expectedOrder, list);
+	}
+
+	@Test
+	void add_atSpecificIndex_ExpectIndexToContainValue() {
+		ArrayList list = new ArrayList<String>();
+		int expectedSize = 3;
+
+		list.add("Hello");
+		list.add("World");
+		list.add(1, ", ");
+
+		assertEquals(expectedSize, list.size());
+		assertEquals(", ", list.get(1));
+
+	}
+
+	@Test
+	void add_atIndex0_ExpectListInReverseOrder() {
+		ArrayList list = new ArrayList<String>();
+		List expectedOrder = Arrays.asList("Geralt", "of", "Rivia");
+
+		list.add(0, "Rivia");
+		list.add(0, "of");
+		list.add(0, "Geralt");
+
+		assertEquals(expectedOrder, list);
+	}
+
+	@Test
+	void addAll_ShouldAddAllItemsToEnd() {
+		ArrayList list = new ArrayList<Integer>();
+		List toAdd = Arrays.asList(3, 4, 5, 6);
+		int expectedSize = 6;
+
+		list.add(1);
+		list.add(2);
+		list.addAll(toAdd);
+
+		assertEquals(expectedSize, list.size());
+		List added = list.subList(2, 6);
+
+		assertEquals(toAdd, added);
+	}
+
+	@Test
+	void addAll_AtSpecificIndex_ShouldAddAllItemsAtIndex() {
+		ArrayList list = new ArrayList<Integer>();
+		List toAdd = Arrays.asList(3, 4, 5, 6);
+		int expectedSize = 6;
+
+		list.add(1);
+		list.add(2);
+		list.addAll(1, toAdd);
+
+		assertEquals(expectedSize, list.size());
+		List added = list.subList(1, 5);
+		assertEquals(toAdd, added);
+	}
+
+	@Test
+	void clear_ShouldMakeListSizeZero() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedSize = 0;
+
+		list.add(1);
+		list.add(2);
+		list.clear();
+
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	void contains_ItemIsInList_ShouldReturnTrue() {
+		ArrayList list = new ArrayList<String>();
+		String item = "42";
+
+		list.add(String.valueOf(item));
+		list.add("Hello World");
+
+		assertTrue(list.contains(item));
+	}
+
+	@Test
+	void contains_ItemIsNotInList_ShouldReturnFalse() {
+		ArrayList list = new ArrayList<String>();
+		String item = "42";
+
+		list.add("39");
+		list.add("Hello World");
+
+		assertFalse(list.contains(item));
+	}
+
+	@Test
+	void indexOf_ItemIsInList_ShouldReturnIndex() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedIndex = 4;
+		int value = 42;
+		list.add(0);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(value);
+		list.add(5);
+
+		assertEquals(expectedIndex, list.indexOf(value));
+	}
+
+	@Test
+	void indexOf_ItemIsNotInList_ShouldReturnNegative1() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedIndex = -1;
+		int value = 39;
+		list.add(0);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+
+		assertEquals(expectedIndex, list.indexOf(value));
+	}
+
+	@Test
+	void isEmpty_EmptyList_ShouldReturnTrue() {
+		ArrayList list = new ArrayList<String>();
+
+		assertTrue(list.isEmpty());
+	}
+
+	@Test
+	void isEmpty_ListHasItems_ShouldReturnFalse() {
+		ArrayList list = new ArrayList<String>();
+		list.add("hello");
+		list.add("world");
+
+		assertFalse(list.isEmpty());
+	}
+
+	@Test
+	void lastIndexOf_ItemAppearsMultipleTimes_ShouldReturnIndex() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedIndex = 5;
+		int value = 42;
+		List toAdd = Arrays.asList(0, 1, value, value, 4, value, 6, 7, 8, 9, 10);
+
+		list.addAll(toAdd);
+
+		assertEquals(expectedIndex, list.lastIndexOf(value));
+	}
+
+	@Test
+	void lastIndexOf_ItemDoesNotAppear_ShouldReturnIndex() {
+		ArrayList list = new ArrayList<Integer>();
+		int expectedIndex = -1;
+		int value = 42;
+		List toAdd = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		list.addAll(toAdd);
+
+		assertEquals(expectedIndex, list.lastIndexOf(value));
+	}
+
+	@Test
+	void remove_Index_ShouldRemoveItemAtIndex() {
+		ArrayList list = new ArrayList<String>();
+		int removeAt = 0;
+		String value = "hello";
+		List toAdd = Arrays.asList(value, "world", "I'm", "java");
+		list.addAll(toAdd);
+		int sizeBeforeRemove = list.size();
+
+		String valueBeforeRemove = (String) list.get(removeAt);
+		boolean removed = list.remove(removeAt);
+		String valueAfterRemove = (String)list.get(removeAt);
+
+		assertEquals(value, removed);
+		assertNotEquals(valueBeforeRemove, valueAfterRemove);
+		assertEquals(sizeBeforeRemove - 1, list.size());
+	}
+
+	@Test
+	void remove_IndexDoesNotExist_ShouldReturnNull() {
+		ArrayList list = new ArrayList<String>();
+		int removeAt = 42;
+		String value = "hello";
+
+		List toAdd = Arrays.asList(value, "world", "I'm", "java");
+		list.addAll(toAdd);
+		int sizeBeforeRemove = list.size();
+
+		boolean removed = list.remove(removeAt);
+
+		assertNull(removed);
+		assertEquals(sizeBeforeRemove, list.size());
+	}
+
+	@Test
+	void remove_ObjectParam_ShouldReturnTrue() {
+		ArrayList list = new ArrayList<String>();
+		int removeAt = 0;
+		String value = "hello";
+		List toAdd = Arrays.asList(value, "world", "I'm", "java");
+		list.addAll(toAdd);
+		int sizeBeforeRemove = list.size();
+
+		String valueBeforeRemove = (String) list.get(removeAt);
+		assertTrue(list.remove(value));
+		String valueAfterRemove =(String) list.get(removeAt);
+
+		assertNotEquals(valueBeforeRemove, valueAfterRemove);
+		assertEquals(sizeBeforeRemove - 1, list.size());
+	}
+
+	@Test
+	void iterator_EmptyListHasNext_ShouldReturnFalse() {
+		ArrayList list = new ArrayList<String>();
+		Iterator<String> itr = list.iterator();
+		assertEquals(itr.hasNext(), false);
+	}
+
+	@Test
+	void iterator_FullListHasNext_ShouldReturnTrue() {
+		ArrayList list = new ArrayList<String>();
+		List toAdd = Arrays.asList("Sterling", "Goodfellow", "Iacas", "Tonwen");
+		list.addAll(toAdd);
+		Iterator<String> itr = list.iterator();
+		assertEquals(itr.hasNext(), true);
+	}
+
+	@Test
+	void iterator_FullListNext_ShouldGetItem() {
+		ArrayList list = new ArrayList<String>();
+		List toAdd = Arrays.asList("Sterling", "Goodfellow", "Iacas", "Tonwen");
+		list.addAll(toAdd);
+		Iterator<String> itrToAdd = toAdd.iterator();
+		for (Object element : list)
+			assertEquals(element, itrToAdd.next());
+	}
+}
